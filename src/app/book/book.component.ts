1be1fb102c6a2ec15dbbeecfd98cd82ec585d7e2
aksjer <aksogut.jerome@gmail.com>
2017-05-24 09:28:40 +0200

    subscription

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,25 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { Book } from '../models/book.model';
 import { CartService } from '../cart.service';
 import { MdSnackBar } from '@angular/material';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent {
+export class BookComponent implements OnDestroy {
 
   @Input() book: Book;
+  sub: Subscription;
 
   constructor(private cartService: CartService, private snackBar: MdSnackBar) { }
 
   addToCart(): void {
-    this.cartService.add(this.book).subscribe(success => {
+    this.sub = this.cartService.add(this.book).subscribe(success => {
       if (success) {
         this.snackBar.open('Added successfully', null, { duration: 1000 });
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
